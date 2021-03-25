03c2e61ad06d89b8f895a5687309fd3039093a61
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-03-25 16:56:28 +0900

    それぞれのnickname削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_043907) do
+ActiveRecord::Schema.define(version: 2021_03_25_075317) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.string "video"
     t.datetime "created_at", precision: 6, null: false
