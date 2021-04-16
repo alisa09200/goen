a83a801e0e1e14064372d507c7e92d7a84fdaf40
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-04-16 12:29:27 +0900

    rootパス変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   get 'main/show'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'tweets#index'
+  #root to: 'tweets#index'
+  root to: 'main#index'
   resources :tweets do
     resources :reservations, only: [:index, :new, :create]
     resources :comments, only: :create
