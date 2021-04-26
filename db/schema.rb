4e3000225d7f528ba540dc020802e9e7c8b1a8fd
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-04-26 19:37:57 +0900

    reservationsテーブルからnameカラム削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_10_075121) do
+ActiveRecord::Schema.define(version: 2021_04_26_103405) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2021_04_10_075121) do
   end
 
   create_table "reservations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.text "content"
     t.datetime "start_time"
     t.integer "user_id"
