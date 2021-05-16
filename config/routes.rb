742899305823e183fb766b7e30d7d39490e51a7c
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-05-16 12:26:14 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,16 @@ Rails.application.routes.draw do
   get 'teacher/show'
   get 'main/index'
   get 'main/show'
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+   }
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy" 
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #root to: 'tweets#index'
   root to: 'main#index'
